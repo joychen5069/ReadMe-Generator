b8ae7e3f336189ff9a004a7357d139e56e87252a
Joy Chen <joychen5069@gmail.com>
2020-04-14 16:09:45 -0400

    added AXIOS package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
-const fs = require("fs")
+const axios = require("axios");
+const fs = require("fs");
 
 
 async function askQuestions() {
@@ -8,18 +9,18 @@ async function askQuestions() {
             message: "What is your name?",
             name: "name",
         })
-       
+
         const { github } = await inquirer.prompt({
             message: "what if your github username?",
             name: "github",
         });
 
-        console.log( github)
+        console.log(github)
 
         const readMe = generateRead(name, github)
-        fs.writeFile("README.md", readMe, function(err) {
+        fs.writeFile("README.md", readMe, function (err) {
             if (err)
-            console.log(err)
+                console.log(err)
         })
 
     } catch (err) {
@@ -32,21 +33,10 @@ async function askQuestions() {
 }
 
 const generateRead = (name, github) => {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <title>Document</title>
-    </head>
-    <body>
-        <div class="container">
-        <h1>${name}</h1>
-        <div class="card">${github}
-        </div>
-    </body>
-    </html>`
+    return
+    `# Welcome to my README.md Generator!
+    
+    This application will allow you to create ReadMe files for your new projects. As a user, when you are promped, you will need to provide your name and your GITHUB url. Using Inquirer and Axios, this application will then create a README that contains your picture, your email, and will list your repositories.`
 }
 askQuestions();
 

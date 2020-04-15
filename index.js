79bd70ea05b714b21303183664a3121f4771b3cd
Joy Chen <joychen5069@gmail.com>
2020-04-15 14:15:25 -0400

    added axios to grab data

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require("fs");
 
-
+//create function to ask questions
 async function askQuestions() {
     try {
+        //use inquirer to ask questions and store
         const { name } = await inquirer.prompt({
             message: "What is your name?",
             name: "name",
         })
 
-        const { github } = await inquirer.prompt({
+        const { user } = await inquirer.prompt({
             message: "what if your github username?",
-            name: "github",
+            name: "user",
         });
 
-        console.log(github)
+        // console.log(user)
+        //user axios to get information
+        const { data } = await axios.get(
+            `https://github.com/${user}`
+        );
 
+        console.log(data) //giant mess. figure out why
+
+        //create constant to have information for README
         const readMe = generateRead(name, github)
         fs.writeFile("README.md", readMe, function (err) {
             if (err)
@@ -27,17 +35,16 @@ async function askQuestions() {
         console.log(err);
     }
 
-
-
-
 }
-
+//create actual README
 const generateRead = (name, github) => {
     return
     `# Welcome to my README.md Generator!
     
     This application will allow you to create ReadMe files for your new projects. As a user, when you are promped, you will need to provide your name and your GITHUB url. Using Inquirer and Axios, this application will then create a README that contains your picture, your email, and will list your repositories.`
 }
+
+//call function to actually run
 askQuestions();
 
 

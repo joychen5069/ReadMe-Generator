b360bb7571fb95f6dee5b1e55695c4a00e32cd43
Joy Chen <joychen5069@gmail.com>
2020-04-15 15:23:34 -0400

    created markdown file, changed questions format

--- index.js
+++ index.js
@@ -1,48 +1,66 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require("fs");
+const util = require("util");
+
+// const generateMarkdown = require("./utils/generateMarkdown")
 
 //create function to ask questions
-async function askQuestions() {
-    try {
-        //use inquirer to ask questions and store
-        const { name } = await inquirer.prompt({
-            message: "What is your name?",
+function askQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
             name: "name",
-        })
+            message: "What is your name?",
+        },
 
-        const { user } = await inquirer.prompt({
-            message: "what if your github username?",
-            name: "user",
-        });
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?",
+        },
+        
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is your project name?",
+        },
 
-        // console.log(user)
+        {
+            type: "input",
+            name: "bio",
+            message: "Tell me a short bio about the project",
+        },
+
+        {
+            type: "input",
+            name: "instal",
+            message: "Did you need any special packages?",
+        },
+
+        {
+            type: "input",
+            name: "license",
+            message: "Does it have any special license?",
+        },
+
+        {
+            type: "input",
+            name: "userStory",
+            message: "What is the user story?",
+        },
+    
+
         //user axios to get information
-        const { data } = await axios.get(
-            `https://github.com/${user}`
-        );
+        // const projectURL = (user, title) => {
+        //     const kebabCaseTitle = title.toLowerCase().split(" ").join("-");
+        //     return ` https://github.com/${user}/${kebabCaseTitle}`
 
-        console.log(data) //giant mess. figure out why
+        // console.log(projectURL) //giant mess. figure out why
 
         //create constant to have information for README
-        const readMe = generateRead(name, github)
-        fs.writeFile("README.md", readMe, function (err) {
-            if (err)
-                console.log(err)
-        })
+    ])    } 
 
-    } catch (err) {
-        console.log(err);
-    }
-
-}
-//create actual README
-const generateRead = (name, github) => {
-    return
-    `# Welcome to my README.md Generator!
-    
-    This application will allow you to create ReadMe files for your new projects. As a user, when you are promped, you will need to provide your name and your GITHUB url. Using Inquirer and Axios, this application will then create a README that contains your picture, your email, and will list your repositories.`
-}
 
 //call function to actually run
 askQuestions();
---
+++ markdown.js
@@ -0,0 +1,26 @@
+//create the actual readme
+
+const generateRead = (name, user) => {
+    return
+    `# Welcome to ${projectName}
+    
+    ## Description
+
+    ## Table of Contents
+
+    ## Installation
+
+    ## Usage
+
+    ## License
+
+    ## Contributing
+
+    ## Tests
+
+    ## Questions
+    
+    <img href=${image} width="100%" height="auto" alt="profile picture"
+    
+    `
+}
